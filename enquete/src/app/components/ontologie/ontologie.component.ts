7d19c782143af793e5e9ca591acbf7df16965204
zahra-ahlal <zahraahlal1997@gmail.com>
2022-04-27 14:42:28 +0200

    Création component Ontologie

---
+++ enquete/src/app/components/ontologie/ontologie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ontologie',
+  templateUrl: './ontologie.component.html',
+  styleUrls: ['./ontologie.component.css']
+})
+export class OntologieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
