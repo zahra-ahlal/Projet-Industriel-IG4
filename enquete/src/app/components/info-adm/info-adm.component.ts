b2dc3d784ec84bf15d55a88771239f5723236f4a
afafbentouil <afaf.bentouil@outlook.fr>
2022-04-27 14:56:17 +0200

    info-adm component

---
+++ enquete/src/app/components/info-adm/info-adm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-adm',
+  templateUrl: './info-adm.component.html',
+  styleUrls: ['./info-adm.component.css']
+})
+export class InfoAdmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
