e39ac732fd512578d4feb1c28669c2cbbac918a7
afafbentouil <afaf.bentouil@outlook.fr>
2022-05-04 12:56:38 +0200

    set up Firebase data base

--- enquete/src/app/app.module.ts
+++ enquete/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 
 import { AppComponent } from './app.component';
 import { InfoAdmComponent } from './components/info-adm/info-adm.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
 
 
 @NgModule({
@@ -17,6 +21,8 @@ import { InfoAdmComponent } from './components/info-adm/info-adm.component';
     AppRoutingModule,
     FormsModule,                               // <========== Add this line!
     //ReactiveFormsModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
