3ffd39868f3199638d55b569d0f94f875f423e6e
zahra-ahlal <zahraahlal1997@gmail.com>
2022-05-10 11:12:30 +0200

    Test parcours Xml

--- enquete/src/app/app.component.ts
+++ enquete/src/app/app.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
-import rdfParser from "rdf-parse";
+import { Component,ChangeDetectionStrategy,ChangeDetectorRef } from '@angular/core';
+
+
+// make availbable your script import
+declare global {
+    var parseXml:(xmlStr:string)=>{}
+}
+//
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    changeDetection:ChangeDetectionStrategy.OnPush
 })
+
+
+
 export class AppComponent {
-  title = 'enquete';
+
+    constructor(
+        private ref:ChangeDetectorRef
+    ){
+
+    }
+    title = 'xmlParser';
+    stringXml = `<?xml version="1.0" encoding="UTF-8"?>
+    <SYNTHESE>
+      <Chimiemoléculaire>
+        <Biomolécules>
+          <Acidesnucléiques>
+            <Amino></Amino>
+          </Acidesnucléiques>
+        </Biomolécules>
+        <Polymères></Polymères>
+      </Chimiemoléculaire>
+    </SYNTHESE>
+    `
+    items:Array<any> = []
+
+    // practical application
+    parseXml:Function = ()=>{
+        let xmlReady:any;
+        xmlReady = parseXml(this.stringXml) // lis le contenu sous format xml
+
+        //renvoie le nom de les enfants de la racine / balise renseignée
+        this.items = Array.from(xmlReady.querySelector("Chimiemoléculaire").children).map((x:any,i)=>{
+            return x.tagName
+        })
+        
+        let enfant = this.items[0]
+        this.items[0] = Array.from(xmlReady.querySelector("Biomolécules").children).map((x:any,i)=>{
+          return x.tagName
+        })
+        
+        console.log(enfant)
+
+        this.ref.detectChanges()
+    }
+    //
 }
