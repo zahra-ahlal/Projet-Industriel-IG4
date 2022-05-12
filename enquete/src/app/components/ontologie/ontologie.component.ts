e27dbbb014e7865cc18a798f5c222f57be82ebae
zahra-ahlal <zahraahlal1997@gmail.com>
2022-05-12 19:43:49 +0200

    Test checkboxes

--- enquete/src/app/components/ontologie/ontologie.component.ts
+++ enquete/src/app/components/ontologie/ontologie.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-ontologie',
   templateUrl: './ontologie.component.html',
   styleUrls: ['./ontologie.component.css']
 })
-export class OntologieComponent implements OnInit {
+export class OntologieComponent {
 
-  constructor() { }
+  form: FormGroup;
+  countries: Array<any> = [
+    { name: 'India', value: 'india' },
+    { name: 'France', value: 'france' },
+    { name: 'USA', value: 'USA' },
+    { name: 'Germany', value: 'germany' },
+    { name: 'Japan', value: 'Japan' }
+  ];
 
-  ngOnInit(): void {
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+     selectedCountries:  new FormArray([])
+    });
+  }
+
+  onCheckboxChange(event: any) {
+    const selectedCountries = (this.form.controls['selectedCountries'] as FormArray);
+    if (event.target.checked) {
+      selectedCountries.push(new FormControl(event.target.value));
+    } else {
+      const index = selectedCountries.controls
+      .findIndex(x => x.value === event.target.value);
+      selectedCountries.removeAt(index);
+    }
+  }
+
+  submit() {
+    console.log(this.form.value);
   }
 
 }
