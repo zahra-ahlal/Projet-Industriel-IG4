73c436de0c9fb9f75a4c6c48f52d9f15d9d99e1f
afafbentouil <afaf.bentouil@outlook.fr>
2022-05-14 11:27:38 +0200

    Transition dans OntologieComponent

--- enquete/src/app/app.module.ts
+++ enquete/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { OntologieComponent } from './components/ontologie/ontologie.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OntologieComponent } from './components/ontologie/ontologie.component';
 
 @NgModule({
   declarations: [
--- enquete/src/app/components/ontologie/ontologie.component.ts
+++ enquete/src/app/components/ontologie/ontologie.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
-
 
 
 @Component({
@@ -8,36 +7,116 @@ import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
   templateUrl: './ontologie.component.html',
   styleUrls: ['./ontologie.component.css']
 })
+
+
 export class OntologieComponent {
 
   form: FormGroup;
-  countries: Array<any> = [
-    { name: 'India', value: 'india' },
-    { name: 'France', value: 'france' },
-    { name: 'USA', value: 'USA' },
-    { name: 'Germany', value: 'germany' },
-    { name: 'Japan', value: 'Japan' }
-  ];
 
-  constructor(fb: FormBuilder) {
+  constructor(private ref:ChangeDetectorRef, fb: FormBuilder){
     this.form = fb.group({
-     selectedCountries:  new FormArray([])
-    });
+      selectedSkills:  new FormArray([])
+     });
   }
 
-  onCheckboxChange(event: any) {
-    const selectedCountries = (this.form.controls['selectedCountries'] as FormArray);
-    if (event.target.checked) {
-      selectedCountries.push(new FormControl(event.target.value));
-    } else {
-      const index = selectedCountries.controls
-      .findIndex(x => x.value === event.target.value);
-      selectedCountries.removeAt(index);
+  title = 'xmlParser';
+
+  stringXml = `<?xml version="1.0" encoding="UTF-8"?>
+  <SYNTHESE>
+    <Chimiemoléculaire>
+      <Biomolécules>
+        <Acidesnucléiques>
+          <Amino></Amino>
+        </Acidesnucléiques>
+      </Biomolécules> 
+      <Polymères>
+      </Polymères>
+    </Chimiemoléculaire>
+  </SYNTHESE>`
+
+  items:Array<any> = []
+  xmlReady:any = parseXml(this.stringXml)
+
+  parsedFile : Array<any> = this.items = Array.from(this.xmlReady.querySelector("SYNTHESE").children).map((x:any,i)=>{
+    return x.tagName
+  })
+
+  // practical application
+  /*parseXml(){
+      console.log(this.xmlReady.querySelector("Chimiemoléculaire"));
+      console.log("FIN " + this.rechercheRec(this.items,"SYNTHESE"))
+  
+      this.ref.detectChanges()
+  }*/
+
+  rechercheRec(tab:any, filtre : String){
+
+    //CAS DE BASE
+    if(this.feuille(tab)){
+      return ""
+    }else{
+      //Au debut le filtre doit etre egale à la racine du coup ; this.xmlReady.querySelector(this.parsedFile[0]).children
+      tab = Array.from(this.xmlReady.querySelector(filtre).children).map((x:any,i)=>{
+        return x.tagName
+      })
+
+      let s=""
+      for(let i = 0; i<tab.length;i++){
+        s+=tab[i] + " - "
+        s+=this.rechercheRec(tab,tab[i])
+      }
+      //console.log("enfant :" + tab + " : "+s);
+      return s
+    }
+
+  }
+
+  feuille(element:any){
+    if(element.length<1)
+      return true
+    else 
+      return false
+  }
+  
+
+  skills = [''];
+
+  addSkill(filtre: string) {
+    if (filtre) {
+      this.items = Array.from(this.xmlReady.querySelector(filtre).children).map((x:any,i)=>{
+        return x.tagName
+      })
+      if(this.skills[0] == ''){
+        this.skills.pop();
+      }
+      for(let  i = 0; i< this.items.length; i++)
+        this.skills.push(this.items[i]);
     }
   }
 
-  submit() {
-    console.log(this.form.value);
+  onCheckboxChange(event: any) {
+
+    const selectedSkills = (this.form.controls['selectedSkills'] as FormArray);
+    
+    if (event.target.checked) {
+      selectedSkills.push(new FormControl(event.target.value));
+    } else {
+      const index = selectedSkills.controls
+      .findIndex(x => x.value === event.target.value);
+      selectedSkills.removeAt(index);
+    }
+
   }
 
+  submit() {
+    console.log(this.form.value['selectedSkills']);
+  }
+
+  getChildren(el: string) : String[] {
+    let children = Array.from(this.xmlReady.querySelector(el).children).map((x:any,i)=>{
+        return x.tagName
+    })
+    return children;
+  }
+  //   Chimiemoléculaire
 }
