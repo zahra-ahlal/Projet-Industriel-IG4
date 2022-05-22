9ac485274073131db48bbfad38f1670b76c9c634
afafbentouil <afaf.bentouil@outlook.fr>
2022-05-22 14:55:04 +0200

    bar progress part 1

--- src/app/components/recap/recap.component.ts
+++ src/app/components/recap/recap.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recap',
   templateUrl: './recap.component.html',
-  styleUrls: ['./recap.component.css']
+  styleUrls: ['./recap.component.scss']
 })
 export class RecapComponent implements OnInit {
 
